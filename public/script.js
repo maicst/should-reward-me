24d862f2cb9e61a4bd35cc824344d8d9a5929b3b
maicst <maicst@proton.me>
2023-11-06 23:49:55 +0100

    mobile support

--- public/script.js
+++ public/script.js
@@ -1,18 +1,32 @@
 function handleRoute() {
     const route = window.location.hash;
-    const contentDiv = document.getElementById('content');
+    const contentDiv = document.getElementById('main-content');
+
+    console.log(route)
 
     let seed = Math.floor(Math.random()*1000)
     let questionHtml = `
-    <h1 id="question">should you reward yourself?</h1>
-    <a id="find-out-btn" href="#/result?s=${seed}">find out</a>
-    `
-    let rationaleHtml = `
-    <h1>random intermittent reinforcement</h1>
-    <p>intermittent reinforcement is the delivery of a reward at irregular intervals, a method that has been determined to yield the greatest effort from the subject. the subject does not receive a reward each time they perform a desired behavior or according to any regular schedule but at seemingly random intervals.
-    </p>
-    <p class="secondary-p">check this on <a id="youtube-link" target="_blank" href="https://youtube.com/shorts/XSnSWdRR5nM?si=CS6pwsN5BgkTEC24">youtube</a>.</p>
-    `
+    <div id="should-reward-container" class="full-width-div">
+        <div id="should-reward-div" class="full-width-div">
+            <h1 id="question">should you reward yourself?</h1>
+        </div>
+        <div id="find-out-div" class="full-width-div">
+            <a id="find-out-btn" href="#/result?s=${seed}">find out</a>
+        </div>
+    </div>
+    <div id="rationale-container" class="full-width-div">
+        <div id="rationale-div" class="full-width-div">
+            <h2>random intermittent reinforcement</h2>
+            <p id="rationale-p">intermittent reinforcement is the delivery of a reward at irregular intervals,
+                a method that has been determined to yield the greatest effort from the subject.
+                the subject does not receive a reward each time they perform a desired behavior or according to any regular schedule
+                but at seemingly random intervals.
+            </p>
+            <p class="secondary-p">check this on
+                <a id="youtube-link" target="_blank" href="https://youtube.com/shorts/XSnSWdRR5nM?si=CS6pwsN5BgkTEC24">youtube</a>.
+            </p>            
+        </div>
+    </div>`
 
     switch (true) {
         case route === '#/':
@@ -43,9 +57,6 @@ function handleRoute() {
                 contentDiv.innerHTML = resHtml;
             }
             break;
-        case route === '#/rationale':
-            contentDiv.innerHTML = rationaleHtml;
-            break
         default:
             contentDiv.innerHTML = questionHtml;
             break;
